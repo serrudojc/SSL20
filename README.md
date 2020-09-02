ba2c985bb502366bff182df410c870401e606bb8
serrudojc <serrudo.jc@gmail.com>
2020-09-02 16:38:10 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# SSL20
+ Cátedra SSL UTN Zuñiga 2020
---
+++ TP1/prueba/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Hello world!\n");
+    int i=0;
+    for(i=0;i<10;i++){
+        printf("%d\n",i);
+    }
+    return 0;
+}
